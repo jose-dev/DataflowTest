66861fe86264bbff9af55d9db12be8ec36032136
Jose Luis Jimenez Casaux <jose.jimenez@MB25940.local>
2017-07-04 17:13:33 +0100

    template working

--- beam/java/bigquery2datastore/README.txt
+++ beam/java/bigquery2datastore/README.txt
@@ -3,3 +3,27 @@ mvn clean install
 gcloud auth application-default login
 
 mvn exec:java -Dexec.mainClass=com.jose.dataflow.MinimalBigqueryToDatastore -Dexec.cleanupDaemonThreads=false -Dexec.args="--project=dev-ocd-eu-datascience --stagingLocation=gs://dataflow-dev-ocd-eu-datascience/xxx --runner=DataflowRunner --tempLocation=gs://tmp-dataflow-dev-ocd-eu-datascience/xxx --dataset=dev-ocd-eu-datascience --namespace=testnamespace --input=dev-ocd-eu-datascience:aaa.small_datastore --keyName=CustomerIdentifier --kind=bqtablerows" -e -X
+
+
+
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+TEMPLATE
+--------
+
+ mvn compile exec:java \
+     -Dexec.mainClass=com.jose.dataflow.TemplateBigqueryToDatastore \
+     -Dexec.cleanupDaemonThreads=false \
+     -Dexec.args="--runner=DataflowRunner \
+                  --project=dev-ocd-eu-datascience \
+                  --stagingLocation=gs://dataflow-dev-ocd-eu-datascience/staging \
+                  --tempLocation=gs://tmp-dataflow-dev-ocd-eu-datascience/tmploc \
+                  --templateLocation=gs://dataflow-dev-ocd-eu-datascience/templates/TemplateBigqueryToDatastore/0.0.1" \
+                  -e -X
+
+
+
+
+ python python/run_template.py
---
+++ beam/java/bigquery2datastore/python/run_template.py
@@ -0,0 +1,65 @@
+"""
+
+
+"""
+
+
+from googleapiclient.discovery import build
+from oauth2client.client import GoogleCredentials
+import pprint
+import time
+
+SLEEPTIME=15
+
+credentials = GoogleCredentials.get_application_default()
+service = build('dataflow', 'v1b3', credentials=credentials)
+
+# Set the following variables to your values.
+JOBNAME = 'testingbq2datastoretemplate'
+PROJECT = 'dev-ocd-eu-datascience'
+VERSION='0.0.1'
+BUCKET='dataflow-dev-ocd-eu-datascience'
+TEMPLATE = 'TemplateBigqueryToDatastore'
+
+
+DATASET = "dev-ocd-eu-datascience"
+NAMESPACE = "testnamespace"
+INPUT = "dev-ocd-eu-datascience:aaa.original"
+#INPUT = "dev-ocd-eu-datascience:aaa.small_datastore"
+KEYNAME = "CustomerIdentifier"
+KIND = "bqtablerows2"
+
+BODY = {
+    "jobName": "{jobname}".format(jobname=JOBNAME),
+    "gcsPath": "gs://{bucket}/templates/{template}/{version}".format(bucket=BUCKET, template=TEMPLATE, version=VERSION),
+    "parameters": {
+        "dataset" : DATASET,
+        "namespace": NAMESPACE,
+        "input" : INPUT,
+        "keyName" : KEYNAME,
+        "kind" : KIND
+    },
+    "environment": {
+        "tempLocation": "gs://{bucket}/temp".format(bucket=BUCKET),
+        "zone": "us-central1-f"
+    }
+}
+
+request = service.projects().templates().create(projectId=PROJECT, body=BODY)
+response = request.execute()
+job_id = response['id']
+
+pprint.pprint(response)
+
+while(True):
+    response = service.projects().jobs().get(projectId=PROJECT, jobId=job_id).execute()
+    # JOB_STATE_RUNNING
+    # JOB_STATE_DONE
+    # JOB_STATE_FAILED
+    pprint.pprint(response)
+    if response['currentState'] != 'JOB_STATE_RUNNING':
+        break
+    else:
+        time.sleep(SLEEPTIME)
+
+
